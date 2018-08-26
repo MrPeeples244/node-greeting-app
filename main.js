d054a38bd78c71482873166db7100cac4f606edd
jordan.crutcher <jordan.crutcher@gmail.com>
2018-08-25 20:49:36 -0500

    node-greeting-app-complete

---
+++ main.js
@@ -0,0 +1,26 @@
+const greetings = require('./greeting.js');
+const farewell = require('./farewell.js');
+
+//"Hello"
+greetings.greetingEnglish();
+
+//"Konichiwa"
+greetings.greetingJapanese();
+
+//"Hola"
+greetings.greetingSpanish();
+
+//"Hallo"
+greetings.greetingIcelandic();
+
+//"Goodbye"
+farewell.farewellEnglish();
+
+//"Sayonara"
+farewell.farewellJapanese();
+
+//"Adios"
+farewell.farewellSpanish();
+
+//"Bless"
+farewell.farewellIcelandic();
